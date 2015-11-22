b7e00e01ae411a02c73418e52eeb8d7266da60fc
irreversible <irreversible>
2015-11-22 09:58:50 +0600

    Restaurant Management System
    Simple Restaurant Management System

---
+++ ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restaurant_Management_System
+{
+    class ConnectionString
+    {
+        public string DBConn = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\RMS_DB.mdf;Integrated Security=True;Connect Timeout=30";
+       
+    }
+}
---
+++ frmMainMenu.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Restaurant_Management_System
+{
+    public partial class frmMainMenu : Form
+    {
+        public frmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void productToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmProduct frm = new frmProduct();
+            frm.Show();
+        }
+
+        private void categoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCategory frm = new frmCategory();
+            frm.Show();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAbout frm = new frmAbout();
+            frm.Show();
+        }
+
+        private void calculatorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Calc.exe");
+        }
+
+        private void notepadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Notepad.exe");
+        }
+
+        private void wordpadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("WordPad.exe");
+        }
+
+        private void mSWordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("WinWord.exe");
+        }
+
+        private void taskManagerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("TaskMgr.exe");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ToolStripStatusLabel4.Text = System.DateTime.Now.ToString();
+        }
+
+        private void registrationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmRegistration frm = new frmRegistration();
+            frm.Show();
+        }
+
+        private void loginDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmLoginDetails frm = new frmLoginDetails();
+            frm.Show();
+        }
+
+        private void profileEntryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCustomers frm = new frmCustomers();
+            frm.Show();
+        }
+
+        private void billingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmSales frm = new frmSales();
+            frm.Show();
+        }
+
+        private void customersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCustomersReport frm = new frmCustomersReport();
+            frm.Show();
+        }
+
+        private void salesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmSalesRecord1 frm = new frmSalesRecord1();
+            frm.Show();
+        }
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmLogin fl = new frmLogin();
+            fl.Show();
+        }
+    }
+}
